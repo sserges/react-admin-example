56011d360581fc9fca133a60f1a19f82d0e556f8
sserges <agounkeserge@gmail.com>
2020-10-09 17:39:24 +0000

    users management components added

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,15 @@ import PostList from './components/PostList';
 import PostCreate from './components/PostCreate';
 import PostEdit from './components/PostEdit';
 
+import UserList from './components/UserList';
+import UserCreate from './components/UserCreate';
+import UserEdit from './components/UserEdit';
+
 function App() {
   return (
     <Admin dataProvider={restProvider('http://localhost:3000')}>
       <Resource name='posts' list={PostList} create={PostCreate} edit={PostEdit} />
+      <Resource name='users' list={UserList} create={UserCreate} edit={UserEdit} />
     </Admin>
   );
 }
